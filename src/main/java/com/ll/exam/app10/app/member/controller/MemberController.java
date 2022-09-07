cfb0e6be4d84c9f99bf1ee979606c9fec5e6c06d
woominsik <9737638@naver.com>
2022-09-07 09:10:06 +0900

    로그인 한 회원의 정보를 프로파일 페이지에서 보여줌

--- src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.ll.exam.app10.app.member.entity.Member;
 import com.ll.exam.app10.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,13 +39,17 @@ public class MemberController {
     }
 
     @GetMapping("/profile")
-    public String showProfile(HttpSession session) {
+    public String showProfile(HttpSession session, Model model) {
         Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
         boolean isLogined = loginedMemberId != null;
 
         if (isLogined == false) {
             return "redirect:/?errorMsg=Need to login!";
         }
+
+        Member loginedMember = memberService.getMemberById(loginedMemberId);
+
+        model.addAttribute("loginedMember", loginedMember);
 
         return "member/profile";
     }
