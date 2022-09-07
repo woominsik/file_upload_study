4e9d95b08dc129164b5e7f2dedaaee5d6304d2f4
woominsik <9737638@naver.com>
2022-09-07 09:06:22 +0900

    회원가입 처리

--- src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -1,14 +1,21 @@
 package com.ll.exam.app10.app.member.controller;
 
+import com.ll.exam.app10.app.member.entity.Member;
+import com.ll.exam.app10.app.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/join")
     public String showJoin() {
         return "member/join";
@@ -16,7 +23,15 @@ public class MemberController {
 
     @PostMapping("/join")
     @ResponseBody
-    public String join() {
+    public String join(String username, String password, String email, MultipartFile profileImg) {
+        Member oldMember = memberService.getMemberByUsername(username);
+
+        if (oldMember != null) {
+            return "이미 가입된 회원입니다.";
+        }
+
+        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+
         return "가입완료";
     }
 }
