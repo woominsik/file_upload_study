6ce7f32ad87aaa528e941c49c05663d86ca0dda2
woominsik <9737638@naver.com>
2022-09-08 22:21:23 +0900

    로그인 요청 시도

---
+++ src/main/java/com/ll/exam/app10/app/member/service/MemberSecurityService.java
@@ -0,0 +1,38 @@
+package com.ll.exam.app10.app.member.service;
+
+import com.ll.exam.app10.app.member.entity.Member;
+import com.ll.exam.app10.app.member.entity.MemberRole;
+import com.ll.exam.app10.app.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MemberSecurityService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).orElseThrow();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if ("admin".equals(username)) {
+            authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+        } else {
+            authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
+        }
+
+        return new User(member.getUsername(), member.getPassword(), authorities);
+    }
+}
--- src/test/java/com/ll/exam/app10/AppTests.java
+++ src/test/java/com/ll/exam/app10/AppTests.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app10;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.exam.app10.app.home.controller.HomeController;
 import com.ll.exam.app10.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
@@ -7,16 +8,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.transaction.Transactional;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -31,6 +40,8 @@ class AppTests {
 
     @Autowired
     private MemberService memberService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @DisplayName("메인화면에서는 안녕이 나와야 한다.")
@@ -61,8 +72,18 @@ class AppTests {
     @DisplayName("user1로 로그인 후 프로필페이지에 접속하면 user1의 이메일이 보여야 한다.")
     @Rollback(false)
     void t3() throws Exception {
+
+        Map<String, String> input = new LinkedHashMap<>();
+        // body에 json 형식으로 회원의 데이터를 넣기 위해서 Map을 이용한다.
+        input.put("username", "user1");
+        input.put("password", "1234");
         ResultActions resultActions = mvc
-                .perform(get())
+                .perform(post("/member/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
     }
 
     @Test
