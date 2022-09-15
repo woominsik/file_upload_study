62b61b05e5149c430791252eb4651798390307b1
woominsik <9737638@naver.com>
2022-09-15 13:39:39 +0900

    컨트롤러에서 현재 로그인한 회원의 정보를 얻는 기존방법

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -3,10 +3,12 @@ package com.ll.exam.app10.app.home.controller;
 import com.ll.exam.app10.app.member.entity.Member;
 import com.ll.exam.app10.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 
@@ -23,5 +25,12 @@ public class HomeController {
     @GetMapping("/about")
     public String showAbout() {
         return "home/about";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/currentUserOrigin")
+    @ResponseBody
+    public Principal currentUserOrigin(Principal principal) {
+        return principal;
     }
 }
