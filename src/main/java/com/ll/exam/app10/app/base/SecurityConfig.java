7ba33b750b746b21578d870d6e3b829efcd1335f
woominsik <9737638@naver.com>
2022-09-15 13:31:10 +0900

    로그인/로그아웃 상태에 따라 페이지 접근 권한 작업

--- src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
+++ src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
@@ -1,7 +1,8 @@
-package com.ll.exam.app10.app.base;
+package com.ll.exam.app10.app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,6 +28,9 @@ public class SecurityConfig {
                         formLogin -> formLogin
                                 .loginPage("/member/login") // GET
                                 .loginProcessingUrl("/member/login") // POST
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/member/logout")
                 );
         return http.build();
     }
