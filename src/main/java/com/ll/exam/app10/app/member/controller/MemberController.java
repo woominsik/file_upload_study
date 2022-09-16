608670b88f2ac6c112e82d92dddc1f9f67468c04
woominsik <9737638@naver.com>
2022-09-16 21:49:59 +0900

    회원정보수정날짜를 캐시키로 사용하여 적절한 때 기존캐시가 지워지도록

--- src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -82,6 +85,12 @@ public class MemberController {
         }
 
         memberService.modify(member, email, profileImg);
+
+        // 기존에 세션에 저장된 MemberContext 객체의 내용을 수정하는 코드 시작
+        context.setModifyDate(member.getModifyDate());
+        Authentication authentication = new UsernamePasswordAuthenticationToken(context, member.getPassword(), context.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        // 기존에 세션에 저장된 MemberContext 객체의 내용을 수정하는 코드 끝
 
         return "redirect:/member/profile";
     }
--- src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
+++ src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
@@ -2,10 +2,12 @@ package com.ll.exam.app10.app.security.dto;
 
 import com.ll.exam.app10.app.member.entity.Member;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,6 +18,8 @@ import java.util.stream.Collectors;
 public class MemberContext extends User implements OAuth2User {
     private final Long id;
     private final String email;
+    @Setter
+    private LocalDateTime modifyDate;
     private final String profileImgUrl;
 
     private Map<String, Object> attributes;
@@ -25,6 +29,7 @@ public class MemberContext extends User implements OAuth2User {
         super(member.getUsername(), member.getPassword(), authorities);
         this.id = member.getId();
         this.email = member.getEmail();
+        this.modifyDate = member.getModifyDate();
         this.profileImgUrl = member.getProfileImgUrl();
     }
 
@@ -50,6 +55,6 @@ public class MemberContext extends User implements OAuth2User {
     }
 
     public String getProfileImgRedirectUrl() {
-        return "/member/profile/img/" + getId() + "?random=" + UUID.randomUUID();
+        return "/member/profile/img/" + getId() + "?cacheKey=" + getModifyDate().toString();
     }
 }
